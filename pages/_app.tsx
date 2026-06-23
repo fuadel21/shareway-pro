e99faceae2a18f89f356c9115f17e6ad9f169bf2
fuadel21 <159694553+fuadel21@users.noreply.github.com>
2026-06-23 23:25:50 +0200

    fix relative imports in app wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import '@/app/globals.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../app/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
