151349ba4151c43400486c89bfafcb1a1a5fb2bc
fuadel21 <159694553+fuadel21@users.noreply.github.com>
2026-06-23 23:21:54 +0200

    feat: add pages app wrapper

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import '@/app/globals.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
